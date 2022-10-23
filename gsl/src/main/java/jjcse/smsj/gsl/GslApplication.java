fae2b4be72b100961fc99ff72490d6b967ec284c
Maljaaa <seungmin4452@gmail.com>
2022-10-23 20:54:00 +0900

    Initial commit

---
+++ gsl/src/main/java/jjcse/smsj/gsl/GslApplication.java
@@ -0,0 +1,13 @@
+package jjcse.smsj.gsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GslApplication.class, args);
+	}
+
+}
---
+++ gsl/src/test/java/jjcse/smsj/gsl/GslApplicationTests.java
@@ -0,0 +1,13 @@
+package jjcse.smsj.gsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
